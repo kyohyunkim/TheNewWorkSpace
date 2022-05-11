52344fcf0c55b49e30ad4592dc1f1e60c49c8a45
kyohyunkim <86403089+kyohyunkim@users.noreply.github.com>
2022-05-11 14:05:05 +0900

    1

--- js/index.js
+++ js/index.js
@@ -4,34 +4,45 @@ $('.thum').ripples({
     perturbance:0.05,
 });
 
-// 서울 날씨 
-$(document).ready(function() {
-    
-    let weatherIcon = {
-    '01' : 'fas fa-sun',
-    '02' : 'fas fa-cloud-sun',
-    '03' : 'fas fa-cloud',
-    '04' : 'fas fa-cloud-meatball',
-    '09' : 'fas fa-cloud-sun-rain',
-    '10' : 'fas fa-cloud-showers-heavy',
-    '11' : 'fas fa-poo-storm',
-    '13' : 'far fa-snowflake',
-    '50' : 'fas fa-smog'
-    };
-    $.ajax({
-    url:'http://api.openweathermap.org/data/2.5/weather?q=seoul&APPID=ee538e4ef0558df86ec8ca228598e2d2&units=metric',
-    dataType:'json',
-    type:'GET',
-    success:function(data){
-    var $Icon = (data.weather[0].icon).substr(0,2);
-    var $Temp = Math.floor(data.main.temp) + 'º';
-    var $city = data.name;
-    $('.CurrIcon').append('<i class="' + weatherIcon[$Icon] +'"></i>');
-    $('.CurrTemp').prepend($Temp);
-    $('.City').append($city);
+const onoff = document.querySelector(".onoff");
+const onoffLi = document.querySelector(".onoff ul li");
+const red = document.querySelectorAll(".title a .red");
+onoff.addEventListener('click', ()=> { 
+    onoffLi.classList.toggle('active');
+    body.classList.toggle('active');
+    for(i=0; i < red.length; i++){
+    red[i].classList.toggle('active');
     }
-    })
-}); 
+});
+
+// 서울 날씨 
+// $(document).ready(function() {
+    
+//     let weatherIcon = {
+//     '01' : 'fas fa-sun',
+//     '02' : 'fas fa-cloud-sun',
+//     '03' : 'fas fa-cloud',
+//     '04' : 'fas fa-cloud-meatball',
+//     '09' : 'fas fa-cloud-sun-rain',
+//     '10' : 'fas fa-cloud-showers-heavy',
+//     '11' : 'fas fa-poo-storm',
+//     '13' : 'far fa-snowflake',
+//     '50' : 'fas fa-smog'
+//     };
+//     $.ajax({
+//     url:'http://api.openweathermap.org/data/2.5/weather?q=seoul&APPID=ee538e4ef0558df86ec8ca228598e2d2&units=metric',
+//     dataType:'json',
+//     type:'GET',
+//     success:function(data){
+//     var $Icon = (data.weather[0].icon).substr(0,2);
+//     var $Temp = Math.floor(data.main.temp) + 'º';
+//     var $city = data.name;
+//     $('.CurrIcon').append('<i class="' + weatherIcon[$Icon] +'"></i>');
+//     $('.CurrTemp').prepend($Temp);
+//     $('.City').append($city);
+//     }
+//     })
+// }); 
 
 
 // 날짜 시간 분 초 실시간
